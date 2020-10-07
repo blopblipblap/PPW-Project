058d557d32c5056ce342b436541af524f034ddfd
Vanessa Emily Agape <vanessaemilyagape9000@gmail.com>
2020-10-07 16:51:18 +0700

    Homepage, Story1, Story3, big update

--- main/views.py
+++ main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def aboutme(request):
+    return render(request, 'main/aboutme.html')
+
+def myexperience(request):
+    return render(request, 'main/myexperience.html')
+
+def story1(request):
+    return render(request, 'main/index.html')
