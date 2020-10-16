fa6dc229ca13975da408d53f28d1198c4fe8a1f0
Vanessa Emily Agape <vanessaemilyagape9000@gmail.com>
2020-10-16 10:57:22 +0700

    Implement models, dynamic url, delete button

--- forms/urls.py
+++ forms/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
-from .views import forms, savematkul, readmatkul
+from .views import forms, savematkul, readmatkul, details, deletematkul
 
 urlpatterns = [
     path('', forms, name='forms'),
     path('savematkul', savematkul),
-    path('readmatkul', readmatkul),
+    path('readmatkul', readmatkul, name='read'),
+    path('details/<int:angka>/', details, name='details'),
+    path('details/<int:angka>/delete/', deletematkul)
 ]
--- forms/views.py
+++ forms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .forms import Input_Form
 from .models import Matkul
@@ -20,3 +20,15 @@ def readmatkul(request):
     response = {'matkuls' : Matkul.objects.all(),
                 'form' : Input_Form}
     return render(request, 'forms/forms.html', response)
+
+def details(request, angka):
+    obj = Matkul.objects.get(id=angka)
+    response = {'detail' : obj}
+    return render(request, 'forms/details.html', response)
+
+def deletematkul(request, angka):
+    obj = get_object_or_404(Matkul, id = angka)
+    if (request.method == 'POST'):
+        obj.delete()
+        return HttpResponseRedirect('/forms/readmatkul')
+    return render(request, 'forms/forms.html')
