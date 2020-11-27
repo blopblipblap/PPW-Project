ddf0c2922f7b5551d9932161249cfb398f3c3cbd
Vanessa Emily Agape <vanessaemilyagape9000@gmail.com>
2020-11-27 14:43:44 +0700

    Repair accordion bug

--- story7/static/js/story7.js
+++ story7/static/js/story7.js
@@ -10,22 +10,28 @@ $(function () {
 $(document).ready(function () {
     var accordionlist = $("#accordion");
     var length = $(accordionlist).children().length;
+    var atasJalan = false;
+    var bawahJalan = false;
 
     $(".atas").click(function (e) {
+        e.stopPropagation();
         e.preventDefault();
         var selected = $(this).parent().parent().parent();
         var selectedIdx = selected.index()
-        if (selectedIdx > 0) {
+        if (selectedIdx > 0) {    
+            atasJalan = true;
             jQuery($(accordionlist).children().eq(selectedIdx - 1)).before(jQuery($(accordionlist).children().eq(selectedIdx)));
             selectedIdx = selectedIdx - 1;
         }
     });
 
     $(".bawah").click(function (e) {
+        e.stopPropagation();
         e.preventDefault();
         var selected = $(this).parent().parent().parent();
         var selectedIdx = selected.index()
         if (selectedIdx < length) {
+            bawahJalan = true;
             jQuery($(accordionlist).children().eq(selectedIdx + 1)).after(jQuery($(accordionlist).children().eq(selectedIdx)));
             selectedIdx = selectedIdx + 1;
         }
