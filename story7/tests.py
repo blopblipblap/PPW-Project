aefcbd36edad0d0df9c327bd3161d0a7009f8a64
Vanessa Emily Agape <vanessaemilyagape9000@gmail.com>
2020-11-27 15:14:04 +0700

    Repair responsive

--- story7/tests.py
+++ story7/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import story7
 
 # Create your tests here.
+class story7(TestCase):
+    #Cek URL
+    def test_urlnya_ada(self):
+        response = Client().get('/story7/')
+        self.assertEquals(response.status_code, 200)
+        
+    #Cek template
+    def test_template_yang_digunakan(self):
+        response = Client().get('/story7/')
+        self.assertTemplateUsed(response, 'story7/story7.html')
