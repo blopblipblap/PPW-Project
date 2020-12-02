f10ce676d87beae7f32f18df366e63769e377327
Vanessa Emily Agape <vanessaemilyagape9000@gmail.com>
2020-12-03 00:45:49 +0700

    Handle imageLinks

--- story8/static/story8_js/story8.js
+++ story8/static/story8_js/story8.js
@@ -8,16 +8,29 @@ $("#search").keyup(function() {
             var array_books = data.items;
             $('.non-boldy').remove();
             for (i = 0; i < array_books.length; i++) {
-                gambar = array_books[i].volumeInfo.imageLinks.smallThumbnail;
+                var html_desktop = "";
+                var html_mobile = "";
                 judul = array_books[i].volumeInfo.title;
                 pengarang = array_books[i].volumeInfo.authors;
-                $('.deskto > tbody:last-child').append(
-                    '<tr class="non-boldy"><td>' + (i+1) + '</td><td><img class="cover" src=' + gambar +
-                    '></td><td>' + judul + 
-                    '</td><td>' + pengarang + '</td></tr>');
-                $('.mobil').append(
-                    '<tr class="non-boldy"><td><img class="cover" src=' + gambar +
-                    '><br><br><b>' + judul + '</b><br><i>' + pengarang + '</i></td></tr>');
+                html_desktop += '<tr class="non-boldy">';
+                html_mobile += '<tr class="non-boldy">';
+                html_desktop += '<td>' + (i+1) + '</td>';
+                if (typeof array_books[i].volumeInfo.imageLinks === 'undefined') {
+                    html_desktop += '<td><img class="cover" src="https://importstance.com/wp-content/uploads/2020/08/noavailable-3640.png"></td>';
+                    html_mobile += '<td><img class="cover" src="https://importstance.com/wp-content/uploads/2020/08/noavailable-3640.png">';
+                } else {
+                    gambar = array_books[i].volumeInfo.imageLinks.smallThumbnail;
+                    html_desktop += '<td><img class="cover" src=' + gambar + '></td>';
+                    html_mobile += '<td><img class="cover" src=' + gambar + '>';
+                }
+                html_desktop += '<td>' + judul + '</td>';
+                html_desktop += '<td>' + pengarang + '</td></tr>';
+                html_mobile += '<br><br>';
+                html_mobile += '<b>' + judul + '</b><br>';
+                html_mobile += '<i>' + pengarang + '</i></td></tr>';
+                
+                $('.deskto > tbody:last-child').append(html_desktop);
+                $('.mobil').append(html_mobile);
                 console.log(judul);
             }
       }});
