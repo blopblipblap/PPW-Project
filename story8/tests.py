e252449a123a84b532f5c5d590543139e2c86ef5
Vanessa Emily Agape <vanessaemilyagape9000@gmail.com>
2020-12-03 20:31:21 +0700

    tests

--- story8/tests.py
+++ story8/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import story8, perantara
 
 # Create your tests here.
+class story8(TestCase):
+    #Cek URL
+    def test_urlnya_ada(self):
+        response = Client().get('/story8/')
+        self.assertEquals(response.status_code, 200)
+    
+    def test_url_data_ada(self):
+        response = Client().get('/story8/data')
+        self.assertEquals(response.status_code, 200)
+
+    #Cek template
+    def test_template_yang_digunakan(self):
+        response = Client().get('/story8/')
+        self.assertTemplateUsed(response, 'story8/story8.html')
+
+    #Cek Views
+    def test_story8_ada(self):
+        found = resolve('/story8/data')
+        self.assertEquals(found.func, perantara)
